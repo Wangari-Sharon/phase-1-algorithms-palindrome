8f17dbffe24f8c96ae2e91815d53195745848b88
Wangari-Sharon <wangarik35@gmail.com>
2022-06-17 08:36:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordArray = word.split("");
+  let reversedWord = wordArray.reverse().join("");
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
